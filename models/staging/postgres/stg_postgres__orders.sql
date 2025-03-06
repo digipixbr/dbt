1915a9f9b56d633fe2643c7ce2e25d30d172b455
Rafael Lohn <rafael.lohn@digipix.com.br>
2025-03-06 12:26:43 +0000

    com archive

---
+++ models/staging/postgres/stg_postgres__orders.sql
@@ -0,0 +1,35 @@
+select
+    -- ids
+    id_loja as shop_id,
+    id_pedido as order_id,
+    id_usuario as user_id,
+
+    -- strings
+    -- numerics
+    round(preco + frete - desconto - adiant_receita, 2) as order_value,
+
+    -- booleans
+    -- dates
+    date(dt_liberacao) as order_date
+
+-- timestamps
+from {{ source("postgres_public", "pedido") }}
+
+union all
+
+select
+    -- ids
+    id_loja as shop_id,
+    id_pedido as order_id,
+    id_usuario as user_id,
+
+    -- strings
+    -- numerics
+    round(preco + frete - desconto - adiant_receita, 2) as order_value,
+
+    -- booleans
+    -- dates
+    date(dt_liberacao) as order_date
+
+-- timestamps
+from {{ source("postgres_archive", "pedido") }}
