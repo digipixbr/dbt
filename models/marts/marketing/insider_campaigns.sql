933531e45bfdc4dd62b700854c3e3c1db0268578
Rafael Lohn <rafael.lohn@digipix.com.br>
2025-04-03 00:44:20 +0000

    fuso fix

--- models/marts/marketing/insider_campaigns.sql
+++ models/marts/marketing/insider_campaigns.sql
@@ -1,17 +1,15 @@
 with
-    analytics as (select * from {{ ref("stg_insider__Email_Campaign_Analytics___Summary") }}),
+    analytics as (
+        select * from {{ ref("stg_insider__Email_Campaign_Analytics___Summary") }}
+    ),
 
     campaigns as (select * from {{ ref("stg_insider__Email_Campaign_List") }}),
 
     insider_campaigns as (
         select *
-            --count(*) as order_items_qty,
-            --round(sum(orders.order_value), 2) as order_total_value
         from analytics
         left join campaigns using (campaign_id)
-        --where orders.order_date is not null
-        --group by orders.shop_id, shops.shop_name, orders.order_date
-        order by start_time_utc DESC, campaign_id DESC
+        order by start_time_utc desc, campaign_id desc
     )
 
 select *
